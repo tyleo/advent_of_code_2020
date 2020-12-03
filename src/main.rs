5e4b4f7dcbe82e27f310b96a6dee6531aa50cac7
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2020-12-02 18:20:20 -0800

    Finish problem 4.

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ extern crate regex;
 mod problem_1;
 mod problem_2;
 mod problem_3;
+mod problem_4;
 mod util;
 
 fn main() {
     println!("problem_1: {}", problem_1::problem_1());
     println!("problem_2: {}", problem_2::problem_2());
     println!("problem_3: {}", problem_3::problem_3());
+    println!("problem_4: {}", problem_4::problem_4());
 }
---
+++ src/problem_4.rs
@@ -0,0 +1,84 @@
+use regex;
+use std::fmt;
+use std::str::FromStr;
+
+use crate::util;
+
+struct PasswordPattern {
+    lower_char_limit: usize,
+    upper_char_limit: usize,
+    char: char,
+    password: String,
+}
+
+lazy_static! {
+    static ref PASSWORD_PATTERN_REGEX: regex::Regex =
+        regex::Regex::new(r"(\d+)-(\d+) (.): (.*)").unwrap();
+}
+
+impl FromStr for PasswordPattern {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let password_captures = PASSWORD_PATTERN_REGEX
+            .captures_iter(s)
+            .nth(0)
+            .ok_or_else(|| "Regex failed to match")?;
+
+        fn parse<T: FromStr>(
+            captures: &regex::Captures,
+            idx: usize,
+            name: &str,
+        ) -> Result<T, String>
+        where
+            T::Err: fmt::Display,
+        {
+            captures
+                .get(idx)
+                .ok_or_else(|| format!("{} not found", name))?
+                .as_str()
+                .parse::<T>()
+                .map_err(|err| format!("{} parse failed: {}", name, err))
+        };
+
+        let (lower_char_limit, upper_char_limit, char, password) = (
+            parse(&password_captures, 1, "lower_char_limit")?,
+            parse(&password_captures, 2, "upper_char_limit")?,
+            parse(&password_captures, 3, "char")?,
+            parse(&password_captures, 4, "password")?,
+        );
+        Ok(Self {
+            lower_char_limit,
+            upper_char_limit,
+            char,
+            password,
+        })
+    }
+}
+
+pub fn problem_4() -> String {
+    let passwords = util::read("input/problem_4_input.txt")
+        .lines()
+        .map(|f| f.parse::<PasswordPattern>().unwrap())
+        .collect::<Vec<_>>();
+
+    return passwords
+        .iter()
+        .filter(|password_pattern| {
+            (if password_pattern.password.as_bytes()[password_pattern.lower_char_limit - 1]
+                == password_pattern.char as u8
+            {
+                1
+            } else {
+                0
+            } + if password_pattern.password.as_bytes()[password_pattern.upper_char_limit - 1]
+                == password_pattern.char as u8
+            {
+                1
+            } else {
+                0
+            }) == 1
+        })
+        .count()
+        .to_string();
+}
