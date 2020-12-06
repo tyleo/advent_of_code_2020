3cdbe4d0cefcdbbfaeb3b274cc58ed4c7c888f0a
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2020-12-06 11:26:48 -0800

    Problem 6 Part 2.

--- src/problem_6_2.rs
+++ src/problem_6_2.rs
@@ -1,3 +1,24 @@
+use crate::util;
+
 pub fn problem_6_2() -> String {
-    "".to_string()
+    util::read("input/problem_6_input.txt")
+        .lines()
+        .fold(vec![([0; u8::MAX as usize], 0)], |mut prev, curr| {
+            let trimmed = curr.trim();
+            match trimmed.is_empty() {
+                true => prev.push(([0; u8::MAX as usize], 0)),
+                false => {
+                    let (questions, people_count) = prev.last_mut().unwrap();
+                    for char in trimmed.bytes() {
+                        questions[char as usize] += 1;
+                    }
+                    *people_count += 1;
+                }
+            };
+            prev
+        })
+        .iter()
+        .map(|(questions, people_count)| questions.iter().filter(|i| *i == people_count).count())
+        .sum::<usize>()
+        .to_string()
 }
