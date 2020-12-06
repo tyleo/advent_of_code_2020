059939655fafa9b0338bff3910064a74f7948ebf
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2020-12-06 11:28:54 -0800

    Improve problem 6.1 solution.

--- src/problem_6_1.rs
+++ src/problem_6_1.rs
@@ -1,21 +1,17 @@
-use std::iter;
-
 use crate::util;
 
 pub fn problem_6_1() -> String {
     util::read("input/problem_6_input.txt")
         .lines()
-        .flat_map(|i| -> Box<dyn Iterator<Item = u8>> {
-            let trimmed = i.trim();
-            match trimmed.is_empty() {
-                true => Box::new(iter::once(' ' as u8)),
-                false => Box::new(trimmed.bytes()),
-            }
-        })
         .fold(vec![[false; u8::MAX as usize]], |mut prev, curr| {
-            match curr == ' ' as u8 {
+            let trimmed = curr.trim();
+            match trimmed.is_empty() {
                 true => prev.push([false; u8::MAX as usize]),
-                false => prev.last_mut().unwrap()[curr as usize] = true,
+                false => {
+                    for char in trimmed.bytes() {
+                        prev.last_mut().unwrap()[char as usize] = true
+                    }
+                }
             };
             prev
         })
