1a86b2765068e3618dfce9b935fa1dbe812b1d7e
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2020-12-10 16:24:45 -0800

    Finish problem 9.

--- src/problem_9_2.rs
+++ src/problem_9_2.rs
@@ -4,15 +4,15 @@ use crate::util;
 
 pub fn problem_9_2() -> String {
     let file = util::read("input/problem_9_input.txt");
-    let lines = file.lines().map(|i| i.parse().unwrap());
+    let lines: Vec<_> = file.lines().map(|i| i.parse().unwrap()).collect();
 
-    let mut window: VecDeque<usize> = lines.clone().take(25).collect();
+    let mut window: VecDeque<usize> = lines.iter().take(25).copied().collect();
 
-    let res = lines.skip(25).find(|curr| {
+    for curr in lines.iter().skip(25).copied() {
         let is_valid = (|| {
             for (iidx, i) in window.iter().enumerate() {
                 for (jidx, j) in window.iter().enumerate() {
-                    if i != j && iidx != jidx && i + j == *curr {
+                    if i != j && iidx != jidx && i + j == curr {
                         return true;
                     }
                 }
@@ -20,10 +20,25 @@ pub fn problem_9_2() -> String {
             return false;
         })();
 
-        window.pop_front();
-        window.push_back(*curr);
-        !is_valid
-    });
+        if !is_valid {
+            for iidx in 0..lines.len() {
+                for jidx in (iidx + 1)..lines.len() {
+                    let sum: usize = lines.iter().skip(iidx).take(jidx - iidx + 1).sum();
+                    if sum == curr {
+                        let range = lines.iter().skip(iidx).take(jidx - iidx + 1);
+                        let min = range.clone().min().unwrap();
+                        let max = range.max().unwrap();
+                        return (min + max).to_string();
+                    } else if sum > curr {
+                        break;
+                    }
+                }
+            }
+        }
 
-    res.unwrap().to_string()
+        window.pop_front();
+        window.push_back(curr);
+    }
+
+    "error".to_string()
 }
