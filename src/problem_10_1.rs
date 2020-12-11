acdd3c822b1b54cc06b4eade4692b64d646b6bb8
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2020-12-10 18:16:41 -0800

    Finish problem 10

--- src/problem_10_1.rs
+++ src/problem_10_1.rs
@@ -1,5 +1,24 @@
 use crate::util;
 
 pub fn problem_10_1() -> String {
-    "".to_string()
+    let mut jolt_ratings: Vec<i32> = util::read("input/problem_10_input.txt")
+        .lines()
+        .map(|i| i.parse().unwrap())
+        .collect();
+    jolt_ratings.sort();
+    let (_, ones, _, threes) = (0..1).chain(jolt_ratings.iter().copied()).fold(
+        (None, 0, 0, 1),
+        |(last, mut ones, mut twos, mut threes), curr| {
+            if let Some(last) = last {
+                match curr - last {
+                    1 => ones += 1,
+                    2 => twos += 1,
+                    3 => threes += 1,
+                    _ => {}
+                }
+            }
+            (Some(curr), ones, twos, threes)
+        },
+    );
+    (ones * threes).to_string()
 }
--- src/problem_10_2.rs
+++ src/problem_10_2.rs
@@ -1,5 +1,48 @@
 use crate::util;
 
 pub fn problem_10_2() -> String {
-    "".to_string()
+    let mut jolt_ratings: Vec<i32> = util::read("input/problem_10_input.txt")
+        .lines()
+        .map(|i| i.parse().unwrap())
+        .collect();
+    jolt_ratings.sort();
+    jolt_ratings
+        .iter()
+        .copied()
+        .fold(
+            (Some(0), 1u128, None, 0u128, None, 0u128),
+            |(last, last_paths, second_last, second_last_paths, third_last, third_last_paths),
+             curr| {
+                let mut curr_paths = 0;
+
+                if let Some(last) = last {
+                    if curr - last <= 3 {
+                        curr_paths += last_paths;
+                    }
+                }
+
+                if let Some(second_last) = second_last {
+                    if curr - second_last <= 3 {
+                        curr_paths += second_last_paths;
+                    }
+                }
+
+                if let Some(third_last) = third_last {
+                    if curr - third_last <= 3 {
+                        curr_paths += third_last_paths;
+                    }
+                }
+
+                (
+                    Some(curr),
+                    curr_paths,
+                    last,
+                    last_paths,
+                    second_last,
+                    second_last_paths,
+                )
+            },
+        )
+        .1
+        .to_string()
 }
